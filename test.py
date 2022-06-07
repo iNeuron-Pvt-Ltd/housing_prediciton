086cb76f5bac1a5d71b438c597507eb448d51ea7
Avnish Yadav <34875169+avnyadav@users.noreply.github.com>
2022-06-07 13:30:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from app_pipeline import TrainingPipeline
 
 if __name__ == '__main__':
     try:
+        
         TrainingPipeline().start_training_pipeline()
     except Exception as e:
         logging.info(e)
